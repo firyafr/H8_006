13208b5cc36b1d15e62dbcd04172693ef2d8122c
firyafr <83689738+firyafr@users.noreply.github.com>
2022-02-27 23:03:09 +0700

    add sesi 3

---
+++ PYTN-KS06-005/PERTEMUAN 3/mod.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def luasPersegi(panjang,lebar) :
+    luas = panjang*lebar
+    return luas
+def kelilingPersegi(panjang+lebar) :
+    return* panjang+lebar
+pip --version
+
+# In[ ]:
+
+
+
+
---
+++ PYTN-KS06-005/PERTEMUAN 3/mod1.py
@@ -0,0 +1,14 @@
+s = "Hacktiv8-PTP Python For Data Science"
+a = [100, 200, 300]
+
+def foo(arg):
+    print(f'arg = {arg}')
+
+class Foo:
+    pass
+
+print(s)
+print(a)
+foo('quux')
+x = Foo()
+print(x)
